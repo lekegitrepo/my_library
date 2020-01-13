d58d68f93c6627e61b7a5365d90f7215c480c77a
lekegitrepo <lekedevrepo@gmail.com>
2020-01-13 07:39:45 -0600

    add app.js file

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, pages, isbn, image = '') {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isbn = isbn;
+  this.image = image;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
